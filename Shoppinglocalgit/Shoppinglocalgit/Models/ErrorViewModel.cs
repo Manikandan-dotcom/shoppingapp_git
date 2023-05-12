32c0c76ac82d264e6b0a491508577688764f8b8f
ManikandanP <pmani.ma1@gmail.com>
2023-05-12 11:58:25 +0530

    first changes

---
+++ Shoppinglocalgit/Shoppinglocalgit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shoppinglocalgit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
